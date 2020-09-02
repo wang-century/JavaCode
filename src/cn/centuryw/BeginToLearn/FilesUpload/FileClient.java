ce76e856de7e3ca778dc28249b758dbd9d0e38f0
master <wsj@centuryw.cn>
2020-09-02 22:55:19 +0800

    null

--- src/cn/centuryw/BeginToLearn/FilesUpload/FileClient.java
+++ src/cn/centuryw/BeginToLearn/FilesUpload/FileClient.java
@@ -6,7 +6,7 @@ import java.net.Socket;
 
 public class FileClient {
     // 要上传的文件路径
-    public static String FILE_SRC = "E:\\SteamLibrary\\steamapps\\workshop\\content\\431960\\2005436556\\preview.png";
+    public static String FILE_SRC = "D:\\Speed.log";
 
     public static void main(String[] args) throws Exception{
         // 创建Socket管道
@@ -17,12 +17,9 @@ public class FileClient {
         BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(os);
         // 读取要上传的文件写出到缓冲字节输出流
         // 发送文件名
-//        PrintStream printStream = new PrintStream(os);
-//        printStream.print("aa.png");
-//        printStream.flush();
-        bufferedOutputStream.write("aa.png".getBytes());
-        Thread.sleep(2000);
-//        socket.shutdownOutput();
+        bufferedOutputStream.write("peed.log".getBytes());
+        bufferedOutputStream.flush();
+        Thread.sleep(500);
         // 发送文件内容
         BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(FILE_SRC));
         byte[] buffer = new byte[1024];
@@ -31,11 +28,7 @@ public class FileClient {
             bufferedOutputStream.write(buffer,0,len);
         }
         bufferedOutputStream.flush();
-        bufferedInputStream.close();
         System.out.println("文件发送完毕");
         socket.shutdownOutput();
-        // 接受服务端的响应
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        System.out.println(bufferedReader.readLine());
     }
 }
--- src/cn/centuryw/BeginToLearn/FilesUpload/ServerReaderThread.java
+++ src/cn/centuryw/BeginToLearn/FilesUpload/ServerReaderThread.java
@@ -15,27 +15,20 @@ public class ServerReaderThread extends Thread{
             // 接受客户端发来的文件
             InputStream inputStream = socket.getInputStream();
             BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-            // 第一次读取文件名
-//            Reader reader = new InputStreamReader(inputStream);
-//            BufferedReader bufferedReader = new BufferedReader(reader);
-//            String fileName = FileServer.FILES_DEST+ File.separator+bufferedReader.readLine();
-//            System.out.println(fileName);
             byte[] buffer = new byte[1024];
             int len;
+            // 第一次读取文件名
             len = bufferedInputStream.read(buffer);
             String fileName = new String(buffer,0,len);
             System.out.println(fileName);
             // 读取文件字节，写出到本地路径
-            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(fileName));
-
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(FileServer.FILES_DEST+File.separator+fileName));
             while ((len=bufferedInputStream.read(buffer))!=-1){
                 bufferedOutputStream.write(buffer,0,len);
             }
             bufferedOutputStream.flush();
             bufferedInputStream.close();
             System.out.println("文件接收完毕");
-            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            bufferedWriter.write("文件接收完毕");
         }catch (Exception e){
             System.out.println(socket.getRemoteSocketAddress()+"断开连接");
         }
